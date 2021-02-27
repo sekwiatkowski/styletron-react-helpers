a7ef8c67b302ca2a00db77ed04c533c95099beae
Sebastian E. Kwiatkowski <sebastian.e.kwiatkowski@outlook.com>
2021-02-27 18:48:11 +0100

    Removed joinClassNames, refactored useCss using additional functions from standard-functions.

--- src/index.js
+++ src/index.js
@@ -1,29 +1,20 @@
 import {useStyletron} from 'styletron-react'
-import {merge} from 'standard-functions'
+import {isArray, isOfLengthOne, merge} from 'standard-functions'
 
 export function useCss() {
     const [css] = useStyletron()
 
     function mergeStyles(...styles) {
-        const firstItem = styles[0]
-        if (Array.isArray(firstItem)) {
-            return mergeStyles(...firstItem)
+        if (isOfLengthOne(styles)) {
+            const firstItem = styles[0]
+
+            if (isArray(firstItem)) {
+                return mergeStyles(...firstItem)
+            }
         }
 
         return css(merge(styles))
     }
 
     return mergeStyles
-}
-
-export function joinClassNames(...items) {
-    const firstItem = items[0]
-
-    if (Array.isArray(firstItem)) {
-        return joinClassNames(...firstItem)
-    }
-
-    return items
-        .filter(n => n)
-        .join(' ')
 }
